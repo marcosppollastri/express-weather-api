a0ec95c1eaa71fd15e69fe29c07ff48ede1a4549
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2022-08-08 14:55:26 -0300

    patch: fixed open weather service bug

--- src/routes/current.router.js
+++ src/routes/current.router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const {getCurrentWeather} = require('../controllers/current.controller');
-const { errorHandler } = require('../middleware/herrorHandler');
 const {normalizeIp} = require('../middleware/normalizeIp');
 
 router.get('/', normalizeIp, getCurrentWeather);
